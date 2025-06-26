7473463030f50d24e0e86aaf084e9d82463be7e6
Diogo Lorran <dgdev9806@gmail.com>
2025-06-26 10:46:17 -0300

    Adição do conceito de método/função

--- Programação-orientada-a-objetos/objetos/index.js
+++ Programação-orientada-a-objetos/objetos/index.js
@@ -1,3 +1,12 @@
 //exemplo de objeto.
-const carro = {marca:"ford",modelo:"Ka",ano:"2015"};
 //os valores dentro  de um objeto são chamados propriedades.
+ const carro = {
+    marca:"ford",
+    modelo:"Ka",
+    ano:"2015" ,
+     placa:"72H7P8",
+    buzina: function(){
+      console.log("Biiiiiiiiii")  
+    }
+    };
+// Objetos também podem ter metodos, Um método e uma função colocada dentro de uma propriedade.
