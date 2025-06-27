13c35b644a26adaf16b013fe91a3705789b49794
Diogo Lorran <dgdev9806@gmail.com>
2025-06-27 09:38:15 -0300

    Adição de exemplo CFB cursos

---
+++ Programação-orientada-a-objetos/Classes/indexCFB.js
@@ -0,0 +1,13 @@
+class Carro{
+    constructor(cor, modelo, ano){
+    this.cor = cor;
+    this.modelo = modelo;
+    this.ano = ano;
+    }
+    
+}
+
+let c1 = new Carro()
+let c2 = new Carro()
+let c3 = new Carro()
+let c4 = new Carro()
