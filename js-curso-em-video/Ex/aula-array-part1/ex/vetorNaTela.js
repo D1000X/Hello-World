083bb53e7b3bcbd6de2eb264223470b63d0a0f55
Diogo Lorran <dgdev9806@gmail.com>
2025-07-02 09:53:21 -0300

    Adição de Variáveis Compostas

--- js-curso-em-video/Ex/aula-array-part1/ex/vetorNaTela.js
+++ js-curso-em-video/Ex/aula-array-part1/ex/vetorNaTela.js
@@ -1,9 +1,20 @@
 let valores = [11,33,4,5];
+valores.push(1)
 valores.sort()
 
-for (const pos in valores) {
+/*for (const pos in valores) {
     console.log(`a posição ${pos} tem valor ${valores[pos]} `)
-}
-/*for (let i = 0; i<valores.length;i++){
+} 
+ for (let i = 0; i<valores.length;i++){
     console.log(`a posição ${i} tem valor ${valores[i]}`)
 }*/
+ 
+let pos = valores.indexOf(2)
+if (pos == -1) {
+    console.log(`o valor não foi encomtrado`)
+}else{
+    console.log(`O valor está na posição ${pos} `)
+}
+ 
+
+
