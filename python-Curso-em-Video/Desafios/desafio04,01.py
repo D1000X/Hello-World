367b07aa28566cc9d1da4b84d60931cdd7b10ad5
D1000X <dgdev9806@gmail.com>
2025-08-11 17:38:45 -0300

    Adiciona novos desafios de validação de entrada e soma de números

---
+++ python-Curso-em-Video/Desafios/desafio04,01.py
@@ -0,0 +1,10 @@
+n = input('Digite um valor:')
+print('Ele contém Numeros?',n .isnumeric())
+print('Ele contém Letras?',n .isalpha())
+print('Ele contém Espaços?',n .isspace())
+print('Ele contém Caracteres Especiais?',n .isalnum())
+print('Ele é inteiro?',n .isdigit())
+
+
+
+  # Utilizando o comando is é suas variações como isnumeric() é etc...
---
+++ python-Curso-em-Video/Desafios/desafio04.py
@@ -0,0 +1,7 @@
+n1 = int(input('digite um numero:'))
+n2 = int(input('digite outro numero:'))
+result = n1 + n2
+print('A soma entre {} é {} da {}'.format(n1, n2, result))
+# Desafio 04 extra de como utilizar o .format()
+ 
+
