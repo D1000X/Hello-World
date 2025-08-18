f6eb3f616901e96589978c5c0ec892196b642ef5
Dg Notebook <208678273+D1000X@users.noreply.github.com>
2025-08-18 10:44:20 -0300

    Aplicação direta do Teorema de Pitágoras sem simplificações. floor: arredonda para baixo (ex: floor(3.9) = 3).  trunc: remove parte decimal sem arredondar (ex: trunc(3.9) = 3)."

---
+++ python-Curso-em-Video/Exercícios/ex05.py
@@ -0,0 +1,7 @@
+import emoji
+print(emoji.emojize("Olá pessoal :pulgar:"))
+#import random
+#num = random.randint(1,10)
+#print(num)
+#Trabalhando com biblliotecas
+# comando para instalar a biblioteca: pip install nome da bliblioteca.
