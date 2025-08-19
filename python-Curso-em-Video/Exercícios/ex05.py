90e2ed7d8d79508231c17a32d3376f2ee33c44f7
D1000X <dgdev9806@gmail.com>
2025-08-19 17:26:04 -0300

    Update ex05.py

--- python-Curso-em-Video/Exercícios/ex05.py
+++ python-Curso-em-Video/Exercícios/ex05.py
@@ -1,5 +1,15 @@
+<<<<<<< HEAD
 # Inportando função da bliblioteca math
 from math import ceil
 num = int(input("Digite um numero:"))
 raiz = ceil(num **(1/2))
 print("A raiz quadrada de {} é {}".format(num,raiz))
+=======
+import emoji
+print(emoji.emojize("Olá pessoal :pulgar:"))
+#import random
+#num = random.randint(1,10)
+#print(num)
+#Trabalhando com biblliotecas
+# comando para instalar a biblioteca: pip install nome da bliblioteca.
+>>>>>>> f6eb3f616901e96589978c5c0ec892196b642ef5
