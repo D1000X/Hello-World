9de1f6a9f7ae14390971ec15e3ada6e68e711916
Dg Notebook <208678273+D1000X@users.noreply.github.com>
2025-08-20 09:10:06 -0300

    adi;áo do estudo da bibllioteca Selenium

---
+++ Selenium/auto.py
@@ -0,0 +1,16 @@
+# automação web com Selenium
+from selenium import webdriver
+# importando o a biblioteca time
+import time
+# abrir o navegador
+navegador = webdriver.Chrome()
+# acessar um site
+navegador.get("https://www.hashtagtreinamentos.com/")
+# colocar o navegador em tela chiea
+navegador.maximize_window()
+# Selecionar um elemento na tela.
+botao_verde = navegador.find_element("Class name" ,"botao-verde")
+# clicar em um elemento
+botao_verde.click()
+
+time.sleep(10)
